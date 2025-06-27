a499e35f511634948a0f4d9173c295c32dc3f88c
zaid444-max <zaidsherzad444@gmail.com>
2025-06-27 14:57:19 +0300

    Update server.js

--- server.js
+++ server.js
@@ -32,6 +32,7 @@ app.use(express.static("public")); // Serve frontend files
 const db = mysql.createPool({
     connectionLimit: 10, // or adjust for your number of users
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT, 
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
